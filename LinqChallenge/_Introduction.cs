6786f59b1e051cbb35b29e2470a39dcf374bfd9e
Daniel Aguirre <daguirre@nexient.com>
2022-06-17 23:35:31 -0500

    Set up _Introduction

---
+++ LinqChallenge/_Introduction.cs
@@ -0,0 +1,98 @@
+﻿namespace LinqChallenge
+{/*   
+        Welcome to my Linq Challenges Repository!
+            
+                    This is brought to you by Daniel Aguirre - https://www.linkedin.com/in/daniel-aguirre-/
+
+
+                While I do recommend you read the whole page - I have included a TLDR at the end - Enjoy! :)
+
+    */
+
+    #region My Goal & Why You Should Use This Repository
+    /*
+
+            My goal is to provide a project where you can practice Linq and/or Test Driven Development.
+
+                    Both of these are valuable skills that any developer can benefit from learning.
+
+            Even if you are already familiar with both of these skills, continuing to hone your craft will help your career.
+
+    */
+    #endregion
+
+    /*                                              MY ONLY REQUEST:
+
+                        I would love to hear any feedback you have after using the repository.
+
+            If I can improve it for future users, or if you just really loved it, I appreciate all feedback.
+
+    */
+
+    #region How Do You Use This Repository?    
+    /*     
+          
+            This depends upon your goal. This repository is set up to support users with three goals in mind.
+
+
+            1) If your goal is only to practice Linq - Use the "Strictly_Linq" branch
+
+                    This branch will have tests pre-created for each method.
+                    This branch does not focus on TDD - so all tests will be uncommented from the start.
+
+
+            2) If your goal is to practice Linq with a minor focus on TDD - Use the "Tests_PreMade" branch 
+
+                    This branch will have (most) tests already created for you. Once you pass a test, 
+                    you will uncomment the next one until you meet all requirements. This is a great
+                    start for TDD, since the idea with Test Driven Development is for you to only 
+                    write enough code to pass the current test that you are working on.
+
+
+            3) If your goal is to practice Test Driven Development - Use the "TDD_Practice" branch
+
+                    This branch will provide you with a list of requirements that you can use to create
+                    your tests. If that seems intimidating, remember that you can reference the other
+                    branches for ideas. Your requirements will be broken down into simple bullet points
+                    that are "One Requirement => One Test".    
+
+                           --------------------------------------------------------------------------
+                           |    When using this branch, please remember to practice TDD properly.   |
+                           |    Create one tests at a time, run it first to ensure it fails.        |
+                           |    After passing one test, create the next one. One at a time.         |
+                           --------------------------------------------------------------------------            
+    */
+    #endregion
+
+    /*
+            If you return to this _Introduction.cs file on another branch, it will have more instructions to get you started.    
+
+
+            Recap / TLDR :
+                
+            Switch to a branch depending on what you want to practice:
+
+
+                    1) Just Linq:
+
+                        "Strictly_Linq" branch - Tests are pre-created, you can attempt to pass tests using Linq.
+
+
+                    2) Linq w/ Intro To Test Driven Development:
+
+                        "Tests_PreMade" branch - Most tests are already created, but commented out. Pass one test at a time!
+
+
+                    3) Test Driven Development Using Linq:
+
+                        "TDD_Practice" branch - Ready to start writing some tests? I'll give you requirements. You create
+                                                one test at a time for each method until you meet all requirements!
+
+
+
+            Thank you for taking the time to use my repository. If you would like to let me know how you felt about my repo, 
+            I greatly appreciate any and all feedback that I receive. You can contact me on:
+
+            LinkedIn: https://www.linkedin.com/in/daniel-aguirre-/
+            Discord:  RedRain#6970
+*/}
