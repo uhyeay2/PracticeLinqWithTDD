713102e712005c326b6b1ec37dae5536329fe4e6
Daniel Aguirre <daguirre@nexient.com>
2022-06-19 01:25:56 -0500

    created methods for Easy.WhereChallenge

--- LinqChallenge.Tests/Easy/WhereChallengeTests.cs
+++ LinqChallenge.Tests/Easy/WhereChallengeTests.cs
@@ -1,15 +1,47 @@
-﻿using LinqChallenge.Domain.Entities;
+﻿using LinqChallenge.Easy;
 
 namespace LinqChallenge.Tests.Easy
 {
     [TestFixture]
     public class WhereChallengeTests
     {
+        /*  Getting Started Instructions
+            
+            TODO: Read Instructions! - You can delete this line after reading 
+                                        - You can see TODO's from ( View -> Task List )         
+
+
+            Below I have created the start of a first test for you. You still need to make a few changes,
+                    such as renaming the test method's name, or making sure that your result is correct.
+
+
+            After your first test (Which should only be asserting one requirement!) you can move on to the next requirement.
+                    - Once you have completed all requirements for a method, you can move on and try another!
+
+
+            If you'd like to see how I created the tests, feel free to take a peek at one of the other branches.
+
+            Coming Soon: Introduction Video
+        */
+
         [Test]
         public void GetPeopleOlderThanEighteen_MakeYourFirstTest()
         {
-            // If you're not sure where to start - You can reference the "Tests_PreMade" branch to see how I created my tests.
-            throw new NotImplementedException();
+            // I gave you a head start. You still need to Finish the test though.
+
+            // Arrange 
+            var challenge = new WhereChallenge();
+
+            // Act
+            var result = challenge.GetPeopleOlderThanEighteen(null!);
+
+            // Assert
+            throw new Exception("You need to Assert your result is correct! && Remove this Exception (: ");
         }
+
+        /*      
+                Hint: You can view your next requirements by going to LinqChallenge.Easy.WhereChallenge.cs
+                      - Find the GetPeopleOlderThanEighteen method, and view the comments above it.
+        */
     }
 }
--- LinqChallenge/Easy/WhereChallenge.cs
+++ LinqChallenge/Easy/WhereChallenge.cs
@@ -5,18 +5,90 @@ namespace LinqChallenge.Easy
 {
     public class WhereChallenge
     {
-        /* GetPeopleOlderThanEighteen - Recommended Requirements          
+        /*  GetPeopleOlderThanEighteen - Recommended Requirements          
         
             These are suggested names for your tests. They should be descriptive enough for you to create an assertion.
 
-            Given_Null_Should_ReturnEmptyCollection
-            Given_EmptyCollection_Should_ReturnEmptyCollection
-            Given_PeopleWhereNoneAreOlderThanEighteen_Should_ReturnEmptyCollection
-            Given_PeopleWhereSomeAreOlderThanEighteen_Should_ReturnOnlyPeopleOlderThanEighteen                        
+            Given_NullOrEmpty_CollectionOfPerson_Should_ReturnEmptyCollection
+            Given_PeopleWhere_NoneAreOlderThanEighteen_Should_ReturnEmptyCollection
+            Given_PeopleWhere_SomeAreOlderThanEighteen_Should_ReturnOnlyPeople_OlderThanEighteen                        
         */
         public IEnumerable<Person> GetPeopleOlderThanEighteen(IEnumerable<Person> people)
         {
             throw new NotImplementedException();
         }
+
+        /*  GetPeopleWhoHaveTheFavoriteColorBlue - Recommended Requirements 
+                 
+            These are suggested names for your tests. They should be descriptive enough for you to create an assertion.
+
+            Given_NullOrEmpty_CollectionOfPerson_Should_ReturnEmptyCollection
+            Given_PeopleWhere_NoneHaveFavoriteColorBlue_Should_ReturnEmptyCollection
+            Given_PeopleWhere_SomeHaveFavoriteColorBlue_Should_ReturnOnlyPeople_WithFavoriteColorBlue
+        */
+        public IEnumerable<Person> GetPeopleWhoHaveTheFavoriteColorBlue(IEnumerable<Person> people)
+        {
+            throw new NotImplementedException();
+        }
+
+        /*  GetPeopleBornInApril - Recommended Requirements 
+                 
+            These are suggested names for your tests. They should be descriptive enough for you to create an assertion.
+
+            Given_NullOrEmpty_CollectionOfPerson_Should_ReturnEmptyCollection
+            Given_PeopleWhere_NoneAreBornInApril_Should_ReturnEmptyCollection
+            Given_PeopleWhere_SomeAreBornInApril_Should_ReturnOnlyPeople_BornInApril
+        */
+        public IEnumerable<Person> GetPeopleBornInApril(IEnumerable<Person> people)
+        {
+            throw new NotImplementedException();
+        }
+
+        /*  GetPeopleWhoseLastNameStartsWithA - Recommended Requirements 
+                 
+            These are suggested names for your tests. They should be descriptive enough for you to create an assertion.
+
+            Given_NullOrEmpty_CollectionOfPerson_Should_ReturnEmptyCollection
+            Given_PeopleWhere_NoLastNameStartsWithA_Should_ReturnEmptyCollection
+            Given_PeopleWhere_SomeLastNamesStartsWithA_Should_ReturnOnlyPeople_WhoseLastNameStartsWithA
+        */
+        public IEnumerable<Person> GetPeopleWhoseLastNameStartsWithA(IEnumerable<Person> people)
+        {
+            throw new NotImplementedException();
+        }
+
+        /*  GetPeopleWhereFullNameContainsLetterO - Recommended Requirements 
+                 
+            These are suggested names for your tests. They should be descriptive enough for you to create an assertion.
+
+            Given_NullOrEmpty_CollectionOfPerson_Should_ReturnEmptyCollection
+            Given_CharProvided_IsNotALetter_Should_ThrowException_WithMessage_CharacterProvidedMustBeALetter
+            Given_PeopleWhere_NoFullName_ContainsCharProvided_Should_ReturnEmptyCollection
+            Given_PeopleWhere_SomeFullNames_ContainCharProvided_Should_ReturnOnlyPeople_WhoseFullNameContainsCharProvided
+        */
+        public IEnumerable<Person> GetPeopleWhereFullNameContainsChar(IEnumerable<Person> people, char c)
+        {
+            throw new NotImplementedException();
+        }
+
+        /*  GetPeopleTallerThan - Recommended Requirements 
+                 
+            These are suggested names for your tests. They should be descriptive enough for you to create an assertion.
+
+            Given_HeightIsNull_Should_ThrowException_WithMessage_InvalidHeightReceived
+            Given_HeightIsZero_Should_ThrowException_WithMessage_InvalidHeightReceived
+            Given_NullOrEmpty_CollectionOfPeople_Should_ReturnEmptyCollection
+            Given_PeopleWhere_NoneAreTaller_ThanHeightProvided_Should_ReturnEmptyCollection
+            Given_PeopleWhere_SomeAreTaller_ThanHeightProvided_Should_ReturnOnlyPeople_TallerThanHeightProvided
+        */
+        public IEnumerable<Person> GetPeopleTallerThan(IEnumerable<Person> people, Length height)
+        {
+            throw new NotImplementedException();
+        }
+
+        //  Are you done?
+        //  If so, Congratulations! You've reached the end of my Easy challenges for Linq's .Where() method.
+        //  Do you have any ideas for more methods/tests? Feel free to make up your own tests/methods.
+
     }
 }
