1e059f28510d837aad29da86d497bd83468bd98c
Daniel Aguirre <daguirre@nexient.com>
2022-06-18 23:18:57 -0500

    Created Tests For GetPeopleOlderThanEighteen

--- LinqChallenge.Tests/Easy/WhereChallengeTests.cs
+++ LinqChallenge.Tests/Easy/WhereChallengeTests.cs
@@ -1,15 +1,71 @@
 ﻿using LinqChallenge.Domain.Entities;
+using LinqChallenge.Domain.Enums;
+using LinqChallenge.Easy;
 
 namespace LinqChallenge.Tests.Easy
 {
     [TestFixture]
     public class WhereChallengeTests
     {
-        [Test]
-        public void GetPeopleOlderThanEighteen_MakeYourFirstTest()
+        private readonly WhereChallenge _challenge = new();
+
+        #region GetPeopleOlderThanEighteen Tests
+
+        [Test, Description("First Test - Return empty collection when input is null.")]
+        public void GetPeopleOlderThanEighteen_Given_Null_Should_ReturnEmptyCollection()
         {
-            // If you're not sure where to start - You can reference the "Tests_PreMade" branch to see how I created my tests.
-            throw new NotImplementedException();
+            Assert.That(_challenge.GetPeopleOlderThanEighteen(null!), Is.Empty);
         }
+
+        //[TestCaseSource(nameof(_emptyPeopleCollections)), Description("Second Test - Return empty collection when input is empty")]
+        //public void GetPeopleOlderThanEighteen_Given_EmptyCollection_Should_ReturnEmptyCollection(IEnumerable<Person> emptyCollection)
+        //{
+        //    Assert.That(_challenge.GetPeopleOlderThanEighteen(emptyCollection), Is.Empty);
+        //}
+
+        //[Test, Description("Third Test - Return empty collection when all people are under eighteen.")]
+        //public void GetPeopleOlderThanEighteen_Given_PeopleWhereNoneAreOlderThanEighteen_Should_ReturnEmptyCollection()
+        //{
+        //    Assert.That(_challenge.GetPeopleOlderThanEighteen(_peopleYoungerThanEighteen), Is.Empty);
+        //}
+
+        //[Test, Description("Fourth Test - Return only people who are 18 or older when input contains people of all ages")]
+        //public void GetPeopleOlderThanEighteen_Given_PeopleWhereSomeAreOlderThanEighteen_Should_ReturnOnlyPeopleOlderThanEighteen()
+        //{
+        //    Assert.That(_challenge.GetPeopleOlderThanEighteen(_peopleOfAllAges), Is.EqualTo(_peopleOlderThanEighteen));
+        //}
+
+        // After completing your tests - go back and refactor your logic! Can you solve this in one line without breaking any tests?
+
+        #endregion
+
+        #region TestCases
+
+        private static readonly IEnumerable<Person> _peopleYoungerThanEighteen = new[]
+        {
+            new Person("John", "Smith", new DateTime(2020, 12, 3), new Length(5, 10), Color.Purple),
+            new Person("Jackson", "Reed", new DateTime(2019, 10, 4), new Length(5, 2), Color.Red),
+            new Person("Bonnie", "Clyde", new DateTime(2018, 9, 4), new Length(5, 2), Color.Blue),
+            new Person("Sarah", "Allen", new DateTime(2017, 8, 4), new Length(5, 2), Color.Orange),
+            new Person("Allen", "Poe", new DateTime(2016, 7, 4), new Length(5, 2), Color.Green),
+        };
+
+        private static readonly IEnumerable<Person> _peopleOlderThanEighteen = new[]
+        {
+            new Person("John", "Smith", new DateTime(1994, 12, 3), new Length(5, 10), Color.Purple),
+            new Person("Jackson", "Reed", new DateTime(1993, 10, 4), new Length(5, 2), Color.Red),
+            new Person("Bonnie", "Clyde", new DateTime(1992, 9, 4), new Length(5, 2), Color.Blue),
+            new Person("Sarah", "Allen", new DateTime(1991, 8, 4), new Length(5, 2), Color.Orange),
+            new Person("Allen", "Poe", new DateTime(1990, 7, 4), new Length(5, 2), Color.Green)
+        };
+
+        private static IEnumerable<Person> _peopleOfAllAges => _peopleYoungerThanEighteen.Concat(_peopleOlderThanEighteen);
+
+        private static readonly object[] _emptyPeopleCollections =
+        {
+            Enumerable.Empty<Person>(), Array.Empty<Person>(), new List<Person>()
+        };
+
+        #endregion
     }
 }
--- LinqChallenge/Easy/WhereChallenge.cs
+++ LinqChallenge/Easy/WhereChallenge.cs
@@ -3,16 +3,21 @@ using System.Linq;
 
 namespace LinqChallenge.Easy
 {
+    /// <summary>
+    /// All tests for this class can be found in the following location:
+    /// LinqChallenge.Tests.Easy.WhereChallengeTests
+    /// </summary>
     public class WhereChallenge
     {
-        /* GetPeopleOlderThanEighteen - Recommended Requirements          
-        
-            These are suggested names for your tests. They should be descriptive enough for you to create an assertion.
+        /* GetPeopleOlderThanEighteen - Tests Details
+
+            Below are the names of your tests. You should try to complete these one at a time.
+            After solving a test, uncomment the next one below it until you have met all requirements.
 
             Given_Null_Should_ReturnEmptyCollection
             Given_EmptyCollection_Should_ReturnEmptyCollection
             Given_PeopleWhereNoneAreOlderThanEighteen_Should_ReturnEmptyCollection
-            Given_PeopleWhereSomeAreOlderThanEighteen_Should_ReturnOnlyPeopleOlderThanEighteen                        
+            Given_PeopleWhereSomeAreOlderThanEighteen_Should_ReturnOnlyPeopleOlderThanEighteen
         */
         public IEnumerable<Person> GetPeopleOlderThanEighteen(IEnumerable<Person> people)
         {
